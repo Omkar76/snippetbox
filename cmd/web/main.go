2ac23b478807613518c8125182e42f01f05f4560
Onkar Jadhav <omjadhav2610@gmail.com>
2023-05-26 16:47:55 +0530

    Use cli arguments for address and static dir path

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Routes
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	// Command line flags
+	addr := flag.String("addr", ":5000", "HTTP network address")
+	staticDir := flag.String("staticDir", "./ui/static", "Path to folder with static files")
+	flag.Parse()
+
+	// Static route
+	fileServer := http.FileServer(http.Dir(*staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :5000")
-	err := http.ListenAndServe(":5000", mux)
+	// Listen
+	log.Println("Starting server on", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
