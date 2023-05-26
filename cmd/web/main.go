fd9e538e3cfa4207adaa6f693fac212a50008ee7
Onkar Jadhav <omjadhav2610@gmail.com>
2023-05-26 20:05:57 +0530

    Improve logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,9 +4,19 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Command line flags
+	addr := flag.String("addr", ":5000", "HTTP network address")
+	staticDir := flag.String("staticDir", "./ui/static", "Path to folder with static files")
+	flag.Parse()
+
+	// Loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	// Routes
@@ -14,17 +24,20 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	// Command line flags
-	addr := flag.String("addr", ":5000", "HTTP network address")
-	staticDir := flag.String("staticDir", "./ui/static", "Path to folder with static files")
-	flag.Parse()
-
 	// Static route
 	fileServer := http.FileServer(http.Dir(*staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Listen
-	log.Println("Starting server on", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	infoLog.Println("Starting server on", *addr)
+	// err := http.ListenAndServe(*addr, mux)
+
+	server := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	err := server.ListenAndServe()
+	errorLog.Fatal(err)
 }
