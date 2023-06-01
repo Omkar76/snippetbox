cd2250692ba83c5f42ba397d0c1954ffb5a3aa95
Onkar Jadhav <omjadhav2610@gmail.com>
2023-06-01 12:47:59 +0530

    Database layer

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,33 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/Omkar76/snippetbox/internal/models"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	snippets *models.SnippetModel
+}
 
 func main() {
 	// Command line flags
 	addr := flag.String("addr", ":5000", "HTTP network address")
-	staticDir := flag.String("staticDir", "./ui/static", "Path to folder with static files")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MYSQL data source name")
 	flag.Parse()
 
 	// Loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
 
-	mux := http.NewServeMux()
+	db, err := openDB(*dsn)
 
-	// Routes
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
-	// Static route
-	fileServer := http.FileServer(http.Dir(*staticDir))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	defer db.Close()
 
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
+		snippets: &models.SnippetModel{DB: db},
+	}
+
+	mux := app.routes()
 	// Listen
 	infoLog.Println("Starting server on", *addr)
 	// err := http.ListenAndServe(*addr, mux)
@@ -38,6 +52,18 @@ func main() {
 		Handler:  mux,
 	}
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
