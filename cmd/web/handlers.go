d98d6f49bb225e0de1251375dc182fc78a8437e9
Onkar Jadhav <omjadhav2610@gmail.com>
2023-06-01 12:47:20 +0530

    DI

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,53 +1,88 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/Omkar76/snippetbox/internal/models"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	// w.Write([]byte("Hello from Snippetbox"))
-	files := []string{
-		"./ui/html/pages/base.go.tpl",
-		"./ui/html/pages/home.go.tpl",
-		"./ui/html/partials/nav.go.tpl",
-	}
+	// // w.Write([]byte("Hello from Snippetbox"))
+	// files := []string{
+	// 	"./ui/html/pages/base.go.tpl",
+	// 	"./ui/html/pages/home.go.tpl",
+	// 	"./ui/html/partials/nav.o.tpl",
+	// }
 
-	ts, err := template.ParseFiles(files...)
+	// ts, err := template.ParseFiles(files...)
 
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// err = ts.ExecuteTemplate(w, "base", nil
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	snippets, err := app.snippets.Latest()
 	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		app.serverError(w, err)
 	}
 
-	if ts.ExecuteTemplate(w, "base", nil) != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%v\n", *snippet)
 	}
 
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+
+		return
+	}
+
+	fmt.Fprintf(w, "%v", *snippet)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Allow", http.MethodPost)
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	// w.Write([]byte("Create a new snippet..."))
 }
